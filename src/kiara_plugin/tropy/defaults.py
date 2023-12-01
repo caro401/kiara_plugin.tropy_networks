949755ecace161ad23704b22bee2d1901c2d0771
Markus Binsteiner <markus@frkl.io>
2023-12-01 10:25:39 +0100

    chore: add initial centrality modules (dummy functionality)

--- src/kiara_plugin/tropy/defaults.py
+++ src/kiara_plugin/tropy/defaults.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
-DEFAULT_UNWEIGHTED_NODE_DEGREE_COLUMN_NAME = "_node_degree_unweighted"
+DEFAULT_UNWEIGHTED_NODE_DEGREE_COLUMN_NAME = "_degree_unweighted"
+DEFAULT_WEIGHTED_NODE_DEGREE_COLUMN_NAME = "_degree_weighted_"
+DEFAULT_UNWEIGHTED_BETWEENNESS_COLUMN_NAME = "_betweenness"
+DEFAULT_WEIGHTED_BETWEENNESS_COLUMN_NAME = "_betweenness_weighted_"
+
+UNWEIGHTED_NODE_DEGREE_TEXT = (
+    """The degree of a node is the number of edges connected to the node."""
+)
+WEIGHTED_NODE_DEGREE_TEXT = """The degree of a node is the number of edges connected to the node. The weight of the edges is taken into account."""
+UNWEIGHTED_NODE_BETWEENNESS_TEXT = """The betweenness centrality for a node v is the fraction of all shortest paths that pass through v."""
+WEIGHTED_NODE_BETWEENNESS_TEXT = """The betweenness centrality for a node v is the fraction of all shortest paths that pass through v. The weight of the edges is taken into account."""
