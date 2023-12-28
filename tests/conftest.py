1bed503f77afc8990f8a5252495fcf55bb9798b1
Markus Binsteiner <markus@frkl.io>
2023-12-28 14:00:58 +0100

    chore: sync project template

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,8 +19,8 @@ import pytest
 
 from kiara.context import KiaraConfig
 from kiara.interfaces.python_api import KiaraAPI
-from kiara.interfaces.python_api.models.job import JobTest
-from kiara.utils.testing import get_tests_for_job, list_job_descs
+from kiara.interfaces.python_api.models.job import JobDesc, JobTest
+from kiara.utils.testing import get_init_job, get_tests_for_job, list_job_descs
 
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 JOBS_FOLDER = Path(os.path.join(ROOT_DIR, "examples", "jobs"))
@@ -36,6 +36,10 @@ def create_temp_dir():
     return instance_path
 
 
+def get_job_alias(job_desc: JobDesc) -> str:
+    return job_desc.job_alias
+
+
 @pytest.fixture
 def kiara_api() -> KiaraAPI:
 
@@ -45,7 +49,28 @@ def kiara_api() -> KiaraAPI:
     return api
 
 
-@pytest.fixture(params=list_job_descs(JOBS_FOLDER))
+@pytest.fixture
+def kiara_api_init_example() -> KiaraAPI:
+    instance_path = create_temp_dir()
+    kc = KiaraConfig.create_in_folder(instance_path)
+    api = KiaraAPI(kc)
+
+    init_job = get_init_job(JOBS_FOLDER)
+    if init_job is None:
+        return api
+
+    results = api.run_job(init_job)
+
+    if not init_job.save:
+        return api
+
+    for field_name, alias_name in init_job.save.items():
+        api.store_value(results[field_name], alias_name)
+
+    return api
+
+
+@pytest.fixture(params=list_job_descs(JOBS_FOLDER), ids=get_job_alias)
 def example_job_test(request, kiara_api) -> JobTest:
 
     job_tests_folder = Path(os.path.join(ROOT_DIR, "tests", "job_tests"))
